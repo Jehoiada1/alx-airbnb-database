c44c2f6e86fd3ea693dd943bcd9905900e42e182
Jehoiada <146882918+Jehoiada1@users.noreply.github.com>
2025-07-06 12:01:20 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,35 @@
+SELECT 
+    bookings.id AS booking_id,
+    bookings.date,
+    users.id AS user_id,
+    users.name,
+    users.email
+FROM 
+    bookings
+INNER JOIN 
+    users ON bookings.user_id = users.id;
+
+SELECT 
+    properties.id AS property_id,
+    properties.name AS property_name,
+    properties.location,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM 
+    properties
+LEFT JOIN 
+    reviews ON properties.id = reviews.property_id;
+
+
+
+SELECT 
+    users.id AS user_id,
+    users.name,
+    bookings.id AS booking_id,
+    bookings.date,
+    bookings.property_id
+FROM 
+    users
+FULL OUTER JOIN 
+    bookings ON users.id = bookings.user_id;
